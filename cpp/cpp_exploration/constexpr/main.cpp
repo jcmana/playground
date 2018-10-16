45f038287d506c08b7bf922c91646e36b091df23
jcmana <mjcmana@gmail.com>
2018-10-16 23:16:47 +0200

    Adding CPP constexpr and std_any into cpp_eploration

---
+++ cpp/cpp_exploration/constexpr/main.cpp
@@ -0,0 +1,15 @@
+/*constexpr.c */
+#include<iostream> 
+
+constexpr long int fib(int n)
+{
+	return (n <= 1) ? n : fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+	// value of res is computed at compile time
+	const long int res = fib(30);
+	std::cout << res;
+	return 0;
+}
---
+++ cpp/cpp_exploration/std_any/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <any>
+
+int main()
+{
+	std::any value;
+
+	value = 35;
+	std::cout << std::any_cast<int>(value) << std::endl;
+	
+	value = "asdf";
+	std::cout << std::any_cast<const char *>(value) << std::endl;
+
+	try
+	{
+		std::any_cast<double>(value);
+	}
+	catch (...)
+	{
+		// exception: std::bad_any_cast 
+	}
+
+	std::getchar();  return 0;
+}
