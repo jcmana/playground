9970b31a99e1765dc52fa8493b011aaa1664b58b
Jiri Manasek <jiri.manasek@tescan.com>
2018-10-18 10:40:01 +0200

    Adding CPP struct aggregate into cpp_exploration

--- cpp/cpp_exploration/struct/struct.cpp
+++ cpp/cpp_exploration/struct/struct.cpp
@@ -1,33 +1,24 @@
-#include <windows.h>
+#include <iostream>
 #include <string>
 
-struct S_InitDescriptor
+struct aggregate
 {
-	HINSTANCE			m_hInstance = nullptr;
-	HWND				m_ParentHwnd = nullptr;
-	UINT				m_CallbackMsg = 0;
-	HICON				m_hIcon = nullptr;
-	UINT				m_ResourceID = 0;
+	int a;
+	std::string b;
 
-	std::wstring		m_WindowName;
-	std::wstring		m_WindowClassName;
-	std::wstring		m_StrTooltip;
-
-	bool				m_Hidden = false;
-	bool				m_Invisible = false;
-
-	std::uint32_t		m_ActionCommandID = 0;
-
-	std::uint32_t		m_ShowIcoTimerUID = 0;
-	std::uint32_t		m_AnimIcoTimerUID = 0;
-	std::uint32_t		m_NgSetLastInputTimeTimerUID = 0;
-	std::uint32_t		m_IcoPollingTimerUID = 0;
-	std::uint32_t		m_AnimationStep = 0;
+	void print()
+	{
+		std::cout << "{ a = " << a << ", b = " << b << " }" << std::endl;
+	}
 };
 
 
 int main()
 {
-    return 0;
+	aggregate x = { 3, "abcd" };	// aggregate initializtion
+
+	x.print();
+
+    std::getchar(); return 0;
 }
 
