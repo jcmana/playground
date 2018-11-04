1f5cffe494ccb692a2a5c11a7ab96120912467e9
jcmana <mjcmana@gmail.com>
2018-11-04 22:51:58 +0100

    [CPP] Correct in-place construction cpp_exploration/manual_constr

--- cpp/cpp_exploration/manual_construction/main.cpp
+++ cpp/cpp_exploration/manual_construction/main.cpp
@@ -1,5 +1,3 @@
-#include <cstdlib>
-
 #include <iostream>
 #include <string>
 #include <memory>
@@ -12,6 +10,20 @@ struct custom_construction
 		m_value = 16;
 	};
 
+	// operator new: memory allocation for requested size
+	void * operator new(std::size_t count)
+	{
+		std::cout << "custom_construction::operator new" << std::endl;
+		return std::malloc(count);
+	}
+
+	// placement operator new: in-place construction without memory allocation
+	void * operator new(std::size_t count, void * ptr)
+	{
+		std::cout << "custom_construction::operator new (with placement)" << std::endl;
+		return ptr;
+	}
+
 	std::string m_name;
 	int m_value;
 };
@@ -19,12 +31,17 @@ struct custom_construction
 int main()
 {
 	// allocate memory for the class
-	custom_construction * cc_ptr = (custom_construction *)(malloc(sizeof(custom_construction)));
+	//custom_construction * cc_ptr = nullptr;
+	custom_construction * cc_ptr = (custom_construction *)(std::malloc(sizeof(custom_construction)));
 
-	// call the constructor manually (:: is required here)
-	cc_ptr->custom_construction::custom_construction("abc");
+	// #1 call the constructor manually (:: is required here)
+	// (warning: this might be Visual C++ specific thing and is not guaranteed by the standard)
+	//cc_ptr->custom_construction::custom_construction("abc");
+
+	// #2 call the constructor manually (calling "placement new")
+	auto * ptr = new(cc_ptr) custom_construction("abc");
 
 	std::cout << cc_ptr->m_name << " : " << cc_ptr->m_value << std::endl;
 
-	return 0;
+	std::getchar(); return 0;
 }
