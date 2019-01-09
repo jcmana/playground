e8dd0e68472607c49fc2fbfcd039286dd04f3fe9
Jiri Manasek <jiri.manasek@tescan.com>
2019-01-09 13:33:30 +0100

    [CPP] cpp_exploration/struct const pointer reading

--- cpp/cpp_exploration/struct/struct.cpp
+++ cpp/cpp_exploration/struct/struct.cpp
@@ -19,7 +19,6 @@ struct pod
 	std::string c;
 };
 
-
 int main()
 {
 	// aggregate initializtion:
@@ -30,13 +29,27 @@ int main()
 	}
 
 	// struct comparison:
-	if (true)
+	if (false)
 	{
 		pod a = { 1, 6.5, "omg" };
 		pod b = { 1, 6.5, "omg" };
 
 		// compile error: no operator ==() found
 		//std::cout << (a == b ? "equal" : "not equal") << std::endl;
+	}
+
+	// const struct * and member access:
+	if (true)
+	{
+		pod a = { 1, 6.5, "omg" };
+
+		const pod * const_ptr = &a;
+
+		// reading is allowed
+		const int val = const_ptr->a;
+
+		// modification is illegal
+		//const_ptr->a = 3;
 	}
 
     std::getchar(); return 0;
