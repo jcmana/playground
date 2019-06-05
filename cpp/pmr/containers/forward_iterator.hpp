ed0eb7ad216e1408be557e7975d6d93a548db8aa
Jiri Manasek <jiri.manasek@tescan.com>
2019-06-05 18:21:47 +0200

    [CPP] Adding documentation for pmr/containers/forward_iterator

--- cpp/pmr/containers/forward_iterator.hpp
+++ cpp/pmr/containers/forward_iterator.hpp
@@ -7,30 +7,46 @@
 namespace pmr {
 namespace containers {
 
+/// \brief		Polymorphic forward iterator.
 template<typename T>
 class forward_iterator
 {
 public:
+	/// \brief		Forward iterator facade class.
+	///
+	/// Forward iterator facade is interface expected to implement actual
+	/// iterator functionality. `forward_iterator` practically servers as
+	/// mapping of `facade` methods onto typical (expected) iterator methods.
 	using facade = forward_iterator_facade<T>;
 
 public:
+	/// \brief		Constructs empty (invalid) `forward_iterator`.
 	forward_iterator();
+	/// \brief		Constructs `forward_iterator` from `facade`.
 	forward_iterator(std::unique_ptr<facade> up_iterator);
+	/// \brief		Constructs copy of `forward_iterator`.
 	forward_iterator(const forward_iterator & other);
 
+	/// \brief		Comparison for equality.
 	template<typename U>
 	friend bool operator ==(const forward_iterator<U> & left, const forward_iterator<U> & right);
 	
+	/// \brief		Comparison for inequality.
 	template<typename U>
 	friend bool operator !=(const forward_iterator<U> & left, const forward_iterator<U> & right);
 
+	/// \brief		Increments the `forward_iterator`.
 	forward_iterator & operator ++();
+	/// \brief		Preincrements the `forward_iterator`.
 	forward_iterator operator ++(int);
 
+	/// \brief		Member access operator.
 	T * operator ->();
+	/// \brief		Dereference operator.
 	T & operator  *();
 
 private:
+	/// \brief		Forward iterator facade implementation unique pointer. Empty means invalid.
 	std::unique_ptr<facade> m_up_iterator;
 };
 
