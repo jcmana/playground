edaf0adc7a9ff9fe147423f8a70109fae8e35b3c
Jiri Manasek <jiri.manasek@tescan.com>
2019-07-09 09:56:46 +0200

    [CPP] Coding styles in cpp_exploration/value_category

--- cpp/cpp_exploration/value_category/value_category.cpp
+++ cpp/cpp_exploration/value_category/value_category.cpp
@@ -6,20 +6,16 @@ int main()
 	// Binding to lvalue:
 	{
 		int n = 1;
-		int& l = n;
-		//int&& r = n;		// error
-
-		__asm nop;
+		int & l = n;
+		//int && r = n;		// error
 	}
 
 	// Binding to rvalue:
 	{
 		//int& l = 1;		// error
-		int&& r = 1;
+		int && r = 1;
 
 		r = r + 3;
-
-		__asm nop;
 	}
 
 	return 0;
