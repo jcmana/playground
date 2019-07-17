f6840e4868df97e92a3168123367f6be0c23b798
Jiri Manasek <jiri.manasek@tescan.com>
2019-07-17 13:55:00 +0200

    [CPP] Adding cpp_exploration/member_function_pointer

---
+++ cpp/cpp_exploration/member_function_pointer/member_function_pointer.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+struct simple
+{
+	void method() const
+	{
+		std::cout << "A::method()" << std::endl;
+	}
+};
+
+struct intf
+{
+	virtual void method() const = 0;
+};
+
+struct impl_a : intf
+{
+	virtual void method() const override
+	{
+		std::cout << "impl_a::method()" << std::endl;
+	}
+};
+
+struct impl_b : intf
+{
+	virtual void method() const override
+	{
+		std::cout << "impl_b::method()" << std::endl;
+	}
+};
+
+int main()
+{
+	// pointer to normal class method
+	{
+		simple a;
+
+		auto ptr = &simple::method;
+		(a.*ptr)();
+	}
+
+	// pointer to polymorphic class method
+	{
+		intf * ia_ptr = new impl_a;
+		intf * ib_ptr = new impl_b;
+
+		auto ptr = &intf::method;
+		(ia_ptr->*ptr)();
+		(ib_ptr->*ptr)();
+
+		delete ia_ptr;
+		delete ib_ptr;
+	}
+
+	// WHAT IN THE ACTUAL FUCK?!
+
+	return 0;
+}
