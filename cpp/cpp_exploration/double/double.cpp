98ff5e0c40382a4e27fac00d5969bc3d89a07c5b
Jiri Manasek <jiri.manasek@tescan.com>
2019-07-24 15:55:33 +0200

    [CPP] Adding cpp_exploration/double

---
+++ cpp/cpp_exploration/double/double.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	double positive_zero = + 0.0;
+	double negative_zero = - 0.0;
+
+	if (positive_zero > negative_zero)
+	{
+		std::cout << "positive_zero > negative_zero" << std::endl;
+	}
+
+	if (positive_zero < negative_zero)
+	{
+		std::cout << "positive_zero < negative_zero" << std::endl;
+	}
+
+	if (positive_zero == negative_zero)
+	{
+		std::cout << "positive_zero == negative_zero" << std::endl;
+	}
+
+	if (std::signbit(positive_zero))
+	{
+		std::cout << "positive_zero is signed" << std::endl;
+	}
+
+	if (std::signbit(negative_zero))
+	{
+		std::cout << "negative_zero is signed" << std::endl;
+	}
+
+	return 0;
+}
