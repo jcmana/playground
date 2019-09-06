59a9335ed90a296f40b1e689b46c50e079b41ae7
Jiri Manasek <jiri.manasek@tescan.com>
2019-09-06 10:51:41 +0200

    [Rust] Adding rust_by_example/primitives

---
+++ rust/rust_by_example/helloworld.rs
@@ -0,0 +1,8 @@
+fn main()
+{
+	println!("Hello, {}!", "world");
+	// println! is a macro
+	// - macro is denoted by the ! postfix
+	// - macro is expanded into its implementation
+	// - prinln support string formatting under the {} syntax
+}
---
+++ rust/rust_by_example/primitives.rs
@@ -0,0 +1,8 @@
+fn main()
+{
+    let number = 4i64;
+    let array = [1, 2, 3];
+
+    println!("number = {}", number);
+    println!("array = {}", array);
+}
