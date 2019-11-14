f2c680fb9fbf92445065f8b06edafd882e839dec
Jiri Manasek <jiri.manasek@tescan.com>
2019-11-14 12:30:38 +0100

    [Rust] More rust primitive types

--- rust/rust_by_example/primitives.rs
+++ rust/rust_by_example/primitives.rs
@@ -2,7 +2,9 @@ fn main()
 {
     let number = 4i64;
     let array = [1, 2, 3];
+    let tuple = (1, "string", false);
 
     println!("number = {}", number);
-    println!("array = {}", array);
+    println!("array = {}", array[0]);
+    println!("tuple = {}", tuple.1);
 }
