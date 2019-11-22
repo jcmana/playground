b24f54f6410bf63477aa981a761924fb65011fb0
Jiri Manasek <jiri.manasek@tescan.com>
2019-11-22 10:35:56 +0100

    [CPP] Adding negative exampl into cpp_exploration/forwarding_references

--- cpp/cpp_exploration/forwarding_references/forwarding_references.cpp
+++ cpp/cpp_exploration/forwarding_references/forwarding_references.cpp
@@ -2,14 +2,19 @@
 #include <utility>
 
 template<class T>
-int f(T && x) 
+int f(T && x) 			// x is a forwarding reference and so can be forwarded
 {			
-	// x is a forwarding reference and so can be forwarded
 	return g(std::forward<T>(x));
 }
 
 template<class T>
-int g(T && x)					
+int g(T && x)			// x is a forwarding reference and so can be forwarded			
+{
+	return x;
+}
+
+template<class T>
+int h(const T && x)		// x is not a forwarding reference, because cv-qualification
 {
 	return x;
 }
@@ -22,6 +27,8 @@ int main()
 	f(i);
 	// argument is rvalue, calls f<int>(int&&), std::forward<int>(x) is rvalue
 	f(0);
+
+
 
 	return 0;
 }
