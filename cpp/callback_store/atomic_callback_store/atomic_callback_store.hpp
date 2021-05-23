71759096f2cdabdb5035597e950f179ef6a80878
Jiri Manasek <jiri.manasek@tescan.com>
2021-05-23 23:22:13 +0200

    [CPP] Making atomic_callback_store movable

--- cpp/callback_store/atomic_callback_store/atomic_callback_store.hpp
+++ cpp/callback_store/atomic_callback_store/atomic_callback_store.hpp
@@ -10,6 +10,15 @@ template<typename T>
 class atomic_callback_store
 {
 public:
+    /// \brief          Default constructor, initializes empty store.
+    atomic_callback_store();
+
+    /// \brief          Move constructor, initilizes from `other`.
+    atomic_callback_store(atomic_callback_store && other) noexcept;
+
+    /// \brief          Move operator, moves from `other`.
+    atomic_callback_store & operator  =(atomic_callback_store & other) noexcept;
+
     /// \copydoc        callback::subscribe(T &)
     atomic_callback_guard<T> subscribe(T callable);
 
@@ -17,12 +26,36 @@ public:
     template<typename ... A>
     void invoke(A && ... args) const;
 
+    template<typename T>
+    friend void swap(atomic_callback_store<T> & lhs, atomic_callback_store<T> & rhs);
+
 private:
     mutable std::mutex m_mutex;
     std::vector<atomic_callback<T>> m_callback_store;
 };
 
 #pragma region atomic_callback_store implementation:
+
+template<typename T>
+atomic_callback_store<T>::atomic_callback_store()
+{
+}
+
+template<typename T>
+atomic_callback_store<T>::atomic_callback_store(atomic_callback_store && other) noexcept :
+    atomic_callback_store()
+{
+    swap(*this, other);
+}
+
+template<typename T>
+atomic_callback_store<T> & 
+atomic_callback_store<T>::operator  =(atomic_callback_store & other) noexcept
+{
+    atomic_callback_store empty;
+    swap(*this, empty);
+    swap(*this, other);
+}
 
 template<typename T>
 atomic_callback_guard<T>
@@ -54,6 +87,16 @@ atomic_callback_store<T>::invoke(A && ... args) const
     {
         callback.invoke(std::forward<A>(args) ...);
     }
+}
+
+template<typename T>
+void swap(atomic_callback_store<T> & lhs, atomic_callback_store<T> & rhs)
+{
+    std::unique_lock lock_lhs(lhs.m_mutex);
+    std::unique_lock lock_rhs(rhs.m_mutex);
+
+    using std::swap;
+    swap(lhs.m_callback_store, rhs.m_callback_store);
 }
 
 #pragma endregion
--- cpp/callback_store/atomic_callback_store/main.cpp
+++ cpp/callback_store/atomic_callback_store/main.cpp
@@ -34,7 +34,7 @@ void function_slow()
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
-void main()
+int main()
 {
     // Simple atomic_callback_store test:
     if (false)
@@ -45,8 +45,8 @@ void main()
         c.invoke(&callback_intf::method);
     }
 
-    // Multi-thread atomic_callback_store test:
-    if (true)
+    // Multi-thread atomic_callback test:
+    if (false)
     {
         callback_intf i;
 
@@ -75,11 +75,47 @@ void main()
         t.join();
     }
 
-    /*
+    // Multi-thread atomic_callback test (inlined):
+    if (false)
+    {
+        std::shared_ptr<std::mutex> sp_mutex = std::make_shared<std::mutex>();
+
+        auto proc = [&sp_mutex]
+        {
+            auto sp_mutex_local = sp_mutex;
+
+            while (sp_mutex_local.use_count() == 2)
+            {
+                std::unique_lock lock(*sp_mutex_local);
+                function_slow();
+            }
+        };
+
+        std::thread t(proc);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+        // Depending on the scheduling of this thread and t, following instruction
+        // releases the shared_ptr, therefore only limited number of invokes will go through
+        {
+            std::unique_lock lock(*sp_mutex);
+
+            if (sp_mutex.use_count() == 2)
+            {
+                sp_mutex.reset();
+            }
+        }
+
+        // This loops almost endlessly, most likely starvation caused by scheduler. There is a
+        // race on mutex locking because function_slow() is called under lock.
+
+        t.join();
+    }
+
     if (true)
     {
         callback_intf i;
-        atomic_callback_store<callback_intf> cs;
+        atomic_callback_store<callback_ref<callback_intf>> cs;
         cs.invoke(&callback_intf::method);
         auto cg = cs.subscribe(i);
         cs.invoke(&callback_intf::method);
@@ -87,6 +123,9 @@ void main()
         auto ch = cs.subscribe(i);
         cs.invoke(&callback_intf::method);
         cs.invoke(&callback_intf::method_slow);
+
+        auto cs_moved = std::move(cs);
+        cs.invoke(&callback_intf::method);
+        cs_moved.invoke(&callback_intf::method);
     }
-    */
 }
