1128c95e969f64d737fa8d25278acd5d738efc60
Jiri Manasek <jiri.manasek@tescan.com>
2021-07-28 14:38:16 +0200

    [CPP] Ensuring overlapping tasks termination in MVC demo

--- qt/mvc/source/Main.cpp
+++ qt/mvc/source/Main.cpp
@@ -89,6 +89,8 @@ int main(int argc, char * argv[])
 
     QObject::connect(pMainWidget, &MainWidget::answerRequested, [&]
     {
+        terminate = true;
+
         if (worker.joinable())
         {
             worker.join();
