76f87cce3ab76bdb45caf91e0c91e18cee18297e
jcmana <mjcmana@gmail.com>
2022-06-26 08:57:08 +0200

    [CPP] Fixing unique_ref after broken merge

--- cpp/shared_ref/shared_ref/unique_ref.hpp
+++ cpp/shared_ref/shared_ref/unique_ref.hpp
@@ -19,12 +19,6 @@ public:
     // !!!
     // MOVE SEMANTICS BREAK THE CLASS AS IT REQUIRES INTRODUCTION OF NULL STATE
 
-    unique_ref(unique_ref && other) noexcept = default;
-    // !!!
-    // MOVE SEMANTICS BREAK THE CLASS AS IT REQUIRES INTRODUCTION OF NULL STATE
-
-    unique_ref(unique_ref && other) noexcept = default;
-
     unique_ref & operator  =(const unique_ref & other) noexcept = delete;
     unique_ref & operator  =(unique_ref && other) noexcept = delete;
 
