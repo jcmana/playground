bb9d7f0cd0c88f7756357886820a6015e53f4f5f
Jiri Manasek <jiri.manasek@tescan.com>
2023-08-16 12:09:06 +0200

    [QT] Adding QComboBox test

---
+++ qt/qcombobox/source/Main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <thread>
+#include <future>
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+
+int main(int argc, char * argv[])
+{
+    QApplication application(argc, argv);
+
+    auto * pComboBox = new QComboBox();
+    pComboBox->show();
+
+    auto procedure = [pComboBox]
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        {
+            auto update = [pComboBox]
+            {
+                pComboBox->addItem("a");
+                pComboBox->addItem("b");
+                pComboBox->addItem("c");
+                pComboBox->addItem("d");
+            };
+            QMetaObject::invokeMethod(pComboBox, update);
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        {
+            auto update = [pComboBox]
+            {
+                pComboBox->clear();
+            };
+            QMetaObject::invokeMethod(pComboBox, update);
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        {
+            auto update = [pComboBox]
+            {
+                pComboBox->addItem("a");
+                pComboBox->addItem("b");
+                pComboBox->addItem("d");
+            };
+            QMetaObject::invokeMethod(pComboBox, update);
+        }
+    };
+    auto future = std::async(std::launch::async, procedure);
+
+    auto change = [](int index)
+    {
+        qDebug("current index changed = %d", index);
+    };
+    QObject::connect(pComboBox, &QComboBox::currentIndexChanged, change);
+
+    return application.exec();
+}
