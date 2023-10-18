45b8d7445c52fba03b068c94be5c86803d3b0be8
Jiri Manasek <jiri.manasek@tescan.com>
2023-10-18 11:21:24 +0200

    [CPP] Adding enum_class into cpp_exploration

---
+++ cpp/cpp_exploration/enum_class/enum_class.cpp
@@ -0,0 +1,12 @@
+enum class E
+{
+    A,
+    B,
+    C,
+    D,
+};
+
+int main()
+{
+    constexpr auto n = sizeof(E);
+}
--- cpp/cpp_exploration/std_filesystem_path/std_filesystem_path.cpp
+++ cpp/cpp_exploration/std_filesystem_path/std_filesystem_path.cpp
@@ -20,6 +20,14 @@ void main()
 
     {
         std::wstring a = L"šě\u03B11";
-        std::string b = u"šě\u03B11";
+        //std::string b = u"šě\u03B11";
+    }
+
+    {
+        std::filesystem::path a = "x/y/z.pdf";
+        std::filesystem::path b = "C:/x/y/z.pdf";
+
+        std::cout << "a.has_root_directory() = " << a.has_root_directory() << std::endl;
+        std::cout << "b.has_root_directory() = " << b.has_root_directory() << std::endl;
     }
 }
