db65cb812f36d27c599123e212ba6a217d06169d
mjcmana <mjcmana@gmail.com>
2025-12-04 17:10:39 +0100

    [CPP] Adding up. reference forwarning into cpp_exploration

--- cpp/cpp_exploration/forwarding_references/forwarding_references.cpp
+++ cpp/cpp_exploration/forwarding_references/forwarding_references.cpp
@@ -19,6 +19,20 @@ int h(const T && x)		// x is not a forwarding reference, because cv-qualificatio
 	return x;
 }
 
+int add(int & a, int & b)
+{
+	const auto result = a + b;
+	a = 0;
+	b = 0;
+	return result;
+}
+
+template<typename ... A>
+int packing_references(A && ... args)
+{
+	return add(std::forward<A>(args) ...);
+}
+
 int main()
 {
 	int i;
@@ -28,7 +42,10 @@ int main()
 	// argument is rvalue, calls f<int>(int&&), std::forward<int>(x) is rvalue
 	f(0);
 
-
+	int x = 7;
+	auto y = std::make_unique<int>();
+	*y = 42;
+	packing_references(x, *y);
 
 	return 0;
 }
